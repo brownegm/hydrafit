a34f95c161f6039eec2d8258f0532ffc418904a2
brownegm <32085030+brownegm@users.noreply.github.com>
2023-10-18 15:34:30 -0700

    data update; new R >=3.5 depends

--- data-raw/data_creation.R
+++ data-raw/data_creation.R
@@ -5,20 +5,21 @@
 ##@author: Marvin
 
 # packages
-
 library(here)
 library(dplyr)
 library(readr)
 
 # load raw
-cebe_efm <- read_csv("~/Downloads/scoffonietal2012_cebe_efm.csv",
+cebe_efm <- read_csv(here("data-raw", "scoffonietal2012_cebe_efm.csv"),
                      col_names = FALSE)
-View(cebe_efm)
 
 names(cebe_efm)<-c("psi", "kl")
 
 cebe_efm <- cebe_efm%>%
-  mutate(psi = psi*-1)
+  mutate(psi = psi*-1)%>%
+  mutate(species = rep("cebe"), .before = psi)
+
+head(cebe_efm)
 
 plot(cebe_efm$kl, cebe_efm$psi)
 
