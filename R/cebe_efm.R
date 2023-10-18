e02ff47adc293cbaff8930407e7a70d29d6587a7
brownegm <32085030+brownegm@users.noreply.github.com>
2023-10-18 15:38:24 -0700

    removed examples and change the link to paper

--- R/cebe_efm.R
+++ R/cebe_efm.R
@@ -7,10 +7,9 @@
 #' \item{psi}{Leaf water potential in MPa}
 #' \item{kl}{Leaf hydraulic conductance measured via evaporative flux. Units : mmol m^-2 s^-1 }
 #' }
-#' @source Extracted from Scoffoni et al. 2011: https://doi-org.stanford.idm.oclc.org/10.1104/pp.111.173856
+#' @source Extracted from Scoffoni et al. 2011: <https://doi-org.stanford.idm.oclc.org/10.1104/pp.111.173856>
 #'
-#' @examples
-#' head(cebe_efm)
+
 "cebe_efm"
 
 cebe_efm
