1fed9a98b935043bc6784f314b9c21f559ec3450
brownegm <32085030+brownegm@users.noreply.github.com>
2023-11-10 12:13:44 -0800

    creating a use example for multiple species

--- data-raw/data_creation.R
+++ data-raw/data_creation.R
@@ -2,28 +2,77 @@
 ## Collected evaporative flux data from Scoffoni et al. 2011
 ## https://doi-org.stanford.idm.oclc.org/10.1104/pp.111.173856
 
-##@author: Marvin
+##@author: Marvin Browne
 
-# packages
+## packages
 library(here)
 library(dplyr)
 library(readr)
 
-# load raw
+## load raw
 cebe_efm <- read_csv(here("data-raw", "scoffonietal2012_cebe_efm.csv"),
                      col_names = FALSE)
 
-names(cebe_efm)<-c("psi", "kl")
+heca_efm <- read_csv(here("data-raw", "scoffonietal2012_heca_efm.csv"),
+                     col_names = FALSE)
 
+quag_efm <- read_csv(here("data-raw", "quag_efm.csv"),
+                     col_names = FALSE)
+
+laca_efm <- read_csv(here("data-raw", "laca_efm.csv"),
+                     col_names = FALSE)
+
+hear_efm <- read_csv(here("data-raw", "hear_efm.csv"),
+                     col_names = FALSE)
+
+## modify for clarity
+
+# update column names
+names(cebe_efm)<-c("psi", "kl")
+names(heca_efm)<-c("psi", "kl")
+names(quag_efm)<-c("psi", "kl")
+names(laca_efm)<-c("psi", "kl")
+names(hear_efm)<-c("psi", "kl")
+
+
+# create each df for CEBE alone and modify so psi is negative and there is
+# a column for species name before psi column
 cebe_efm <- cebe_efm%>%
   mutate(psi = psi*-1)%>%
   mutate(species = rep("cebe"), .before = psi)
 
+heca_efm <- heca_efm%>%
+  mutate(psi = psi*-1)%>%
+  mutate(species = rep("heca"), .before = psi)
+
+quag_efm <- quag_efm%>%
+  mutate(psi = psi*-1)%>%
+  mutate(species = rep("quag"), .before = psi)
+
+laca_efm <- laca_efm%>%
+  mutate(psi = psi*-1)%>%
+  mutate(species = rep("laca"), .before = psi)
+
+hear_efm <- hear_efm%>%
+  mutate(psi = psi*-1)%>%
+  mutate(species = rep("hear"), .before = psi)
+
+# check the changes
 head(cebe_efm)
 
-plot(cebe_efm$kl, cebe_efm$psi)
+# plot to make sure
+plot(cebe_efm$kl~cebe_efm$psi, xlim=c(0,-7))
+
+## combine all dataframes into one for multi-species comparisons
+
+scof2012 <-do.call(rbind, list(cebe_efm, heca_efm, quag_efm, laca_efm,hear_efm))
 
 ## save data
-use_data(cebe_efm,
+usethis::use_data(cebe_efm,
          version = 3,
          overwrite = T)# sometimes things change
+
+usethis::use_data(scof2012,
+         version = 3,
+         overwrite = T)
+
