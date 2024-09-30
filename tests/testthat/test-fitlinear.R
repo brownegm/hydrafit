33c8689baddde32e39b6d70183dfe226032bf7ad
brownegm <32085030+brownegm@users.noreply.github.com>
2024-09-29 22:22:05 -0700

    test update

---
+++ tests/testthat/test-fitlinear.R
@@ -0,0 +1,23 @@
+# test that linear fits works
+testthat::test_that("linear fit is correct", {
+# create test values and data frame
+A = 1
+B = 2
+
+psi <-  rnorm(100,3,1)
+
+kl <- hydrafit::Linear(A=A, B=B, psi = psi)
+
+test_df <- data.frame(psi=psi, kl=kl)
+
+# Run fit_linear function
+suppressWarnings({
+  linear_fits = fit_linear(test_df, hydrafit::Linear)
+})
+# Test that the function predicts the correct values
+Apred = round(linear_fits$A, 1)
+Bpred = round(linear_fits$B, 1)
+
+expect_equal(Apred, A)
+expect_equal(Bpred, B)
+})
