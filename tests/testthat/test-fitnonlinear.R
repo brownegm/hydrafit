e79dad0a36be47af1ef66983fdd6fce7b0135fe5
brownegm <32085030+brownegm@users.noreply.github.com>
2024-10-07 10:25:14 -0700

    trying to just use data I already have
    update example but need to check errors

--- tests/testthat/test-fitnonlinear.R
+++ tests/testthat/test-fitnonlinear.R
@@ -9,28 +9,66 @@ C = 0.05
 
 psi <-  rnorm(100,3,1)
 
-kl_exp <- hydrafit::Exponential(A=A, B=B, psi = psi)
-kl_exp2 <- hydrafit::Exponential2(A=A, B=B, C=C, psi = psi)
-kl_sig <- hydrafit::Sigmoidal(A=A, B=B, Xo = C, psi = psi)
-kl_log <- hydrafit::Logistic(A=A, B=B, Xo = C, psi = psi)
+kl_exp <- hydrafit::Exponential(A = A, B = B, psi = psi)
+kl_exp2 <- hydrafit::Exponential2(A = A, B = B, C = C, psi = psi)
+kl_sig <- hydrafit::Sigmoidal(A = A, B = B, Xo = C, psi = psi)
+kl_log <- hydrafit::Logistic(A = A, B = B, Xo = C, psi = psi)
 
 test_df_exp <- data.frame(psi=psi, kl=kl_exp)
 test_df_exp2 <- data.frame(psi=psi, kl= kl_exp2)
 test_df_sig <- data.frame(psi=psi, kl=kl_sig)
 test_df_log <- data.frame(psi=psi, kl=kl_log)
 
+cebe_test <- scof2012|>dplyr::filter(species=="cebe")
 # Run fit_linear function
-suppressWarnings({
-  exp_fits = fit_nonlinear(input_df = test_df_exp, model_type = "exp")
-  exp2_fits = fit_nonlinear(input_df = test_df_exp2, model_type = "exp2")
-  sig_fits = fit_nonlinear(input_df = test_df_sig, model_type = "sig")
-  log_fits = fit_nonlinear(input_df = test_df_log, model_type = "log")
-})
-# Test that the function predicts the correct values
-Apred = round(linear_fits[["A"]], 1)
-Bpred = round(linear_fits[["B"]], 1)
+suppressWarnings(### TRY USING "TRY()" HERE
+try({
+  exp_fits = fit_nonlinear(input_df = cebe_test, model_type = "exp")
+  exp2_fits = fit_nonlinear(input_df = cebe_test, model_type = "exp2")
+  sig_fits = fit_nonlinear(input_df = cebe_test, model_type = "sig")
+  log_fits = fit_nonlinear(input_df = cebe_test, model_type = "log")
+}, silent = TRUE
+  )
+)
 
-print(Apred)
+
+# Test that the function predicts the correct values
+# Apred = round(exp_fits[["A"]], 1)
+# Bpred = round(exp_fits[["B"]], 1)
+# #exponential 2
+# Apred_exp2 = round(exp2_fits[["A"]], 1)
+# Bpred_exp2 = round(exp2_fits[["B"]], 1)
+# Cpred_exp2 = round(exp2_fits[["C"]], 1)
+# # sigmoidal
+# Apred_sig = round(exp_fits[["A"]], 1)
+# Bpred_sig = round(exp_fits[["B"]], 1)
+# Cpred_sig = round(exp_fits[["C"]], 1)
+# # logistic
+# Apred_logistic = round(exp_fits[["A"]], 1)
+# Bpred_logistic = round(exp_fits[["B"]], 1)
+# Cpred_logistic = round(exp_fits[["C"]], 1)
+
+
+
+# test that the modelled values are equalled to the values I put in
 expect_equal(Apred, A)
 expect_equal(Bpred, B)
+#exp2
+expect_equal(Apred, A)
+expect_equal(Bpred, B)
+expect_equal(Cpred, C)
+#sigmoidal
+expect_equal(Apred, A)
+expect_equal(Bpred, B)
+expect_equal(Cpred, C)
+#logistic
+expect_equal(Apred, A)
+expect_equal(Bpred, B)
+expect_equal(Cpred, C)
 })
+
+# I could test for the closest fitting value to the Kmax that I know?
+# Should I just be testing the function itself or all together?
+#
+## Kmax values from Scoffoni et al. 2012
+#cebe4.37, heca5.74, quag3.96, laca11.4, hear20.7
