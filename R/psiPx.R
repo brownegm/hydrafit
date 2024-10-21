663146bd88615760b960ebd60325298390b0faf9
brownegm <32085030+brownegm@users.noreply.github.com>
2024-10-21 09:23:37 -0700

    psipx test update, add fx to namespace

--- R/psiPx.R
+++ R/psiPx.R
@@ -8,50 +8,67 @@
 #'
 #' @return Returns a function which takes model parameters,"px", and "max_cond_at" as parameters.
 #'
-#' @noRd
+#' @example
+#' # create function for estimating pX with linear formula
+#' psi.px <- psiPx(fx_type = "Linear")
+#' A <- 2
+#' B <- 0.5
+#'
+#' psi_at_p50_linear <- psi.px(A=A, B=B, px = 0.5, max_cond_at = 0)
+#'
+#' psi_at_p50_linear
+#' @export
 
 psiPx <- function(fx_type = character()) {
 
   if (fx_type == "Linear") {
 
     \(A, B, px = 0.5, max_cond_at = 0) {
+      px_op <- 1-px
+
       max_c <- (B * max_cond_at) + A
 
-      psi.px <- ((px * max_c) - A) / (B)
+      psi.px <- ((px_op * max_c) - A) / (B)
 
-      return(psi.px)
+      return(list(psi.px, max_c))
 
     }
 
   } else if (fx_type == "exp") {
     \(A, B, px = 0.5, max_cond_at = 0) {
 
+      px_op <- 1-px
+
       max_c <- A * exp(-B * max_cond_at)
 
-      psi.px <- (log((px * max_c) / A)) / (-B)
+      psi.px <- (log((px_op * max_c) / A)) / (-B)
 
-      return(psi.px)
+      return(list(psi.px, max_c))
     }
 
   } else if (fx_type == "exp2") {
     \(A, B, C, px = 0.5, max_cond_at = 0) {
 
+      px_op <- 1-px
+
       max_c <- C + (A * exp(-B * max_cond_at))
 
-      psi_px <- -((log((( px * max_c) - C) / A)) / (B))
+      psi_px <- -((log(((px_op * max_c) - C) / A)) / (B))
 
-      return(psi_px)
+      return(list(psi_px, max_c))
 
     }
 
   } else if (fx_type == "log") {
     \(A, B, C, px = 0.5, max_cond_at = 0) {
 
+      px_op <- 1-px
+
       max_c <- (A) / (1 + (max_cond_at / C) ^ B)
 
-      psi_px <- (C) * (((A) / (px * max_c) - 1) ^ (1 / B))
+      psi_px <- (C) * (((A) / (px_op * max_c) - 1) ^ (1 / B))
 
-      return(psi_px)
+      return(list(psi_px, max_c))
     }
 
   } else{
@@ -59,11 +76,13 @@ psiPx <- function(fx_type = character()) {
 
     \(A, B, C, px = 0.5, max_cond_at = 0) {
 
+      px_op <- 1-px
+
       max_c <- (A) / (1 + exp(-((max_cond_at - C) / B)))
 
-      psi_px <- -B * (A - (px * max_c) - log(px * max_c)) + C
+      psi_px <- -B * (A - (px_op * max_c) - log(px_op * max_c)) + C
 
-      return(psi_px)
+      return(list(psi_px, max_c))
 
     }
 
--- tests/testthat/test-psiPx.R
+++ tests/testthat/test-psiPx.R
@@ -1,1 +1,8 @@
 ## Max C should equal the value from the fit when "max_cond_at"=0
+ psi.px <- psiPx(fx_type = "Linear")
+ A <- 2
+ B <- 0.5
+
+psi_at_p50_linear <- psi.px(A=A, B=B, px = 0.5, max_cond_at = 0)
+
+psi_at_p50_linear
