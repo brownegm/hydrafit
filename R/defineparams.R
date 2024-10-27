6b145fffd63238333994eaf97596a57f3bd6e1ff
brownegm <32085030+brownegm@users.noreply.github.com>
2024-10-26 22:54:26 -0700

    Update defineparams.R
    Added define linear params to the define params fax

--- R/defineparams.R
+++ R/defineparams.R
@@ -132,13 +132,38 @@ define_parsE2 <- function(input_df) {
   ))
 }
 
+#' @rdname define_pars
+#' @export define_parslin
+define_parslin<-function(input_df){
+
+lm(input_df$kl ~ input_df$psi)-> fita #a normal linear regression gave the best starting parameters
+
+  pars = list(
+    A = summary(fita)$coeff[1, 1],
+    B = summary(fita)$coeff[2, 1],
+    sd = 1
+    )
+  par_lo1 = list(
+    A = summary(fita)$coeff[1, 1] * 0.1,
+    B = summary(fita)$coeff[2, 1] * 2,
+    sd = 0.005
+  )
+  par_hi1 = list(
+    A = summary(fita)$coeff[1, 1] * 2,
+    B = summary(fita)$coeff[2, 1] * 0.1,
+    sd = 20
+  )
+
+return(list(pars, par_lo1, par_hi1))
+
+}
 
 define_pars <- function(input_df, model_type){
 
   par_fx <- ifelse(model_type=="log", define_parsL,
                           ifelse(model_type=="exp", define_parsE,
                                  ifelse(model_type=="exp2", define_parsE2,
-                                        ifelse(model_type=="sig", define_parsS))))
+                                        ifelse(model_type=="sig", define_parsS, define_parslin))))
 
   parameters <- par_fx(input_df)
 
