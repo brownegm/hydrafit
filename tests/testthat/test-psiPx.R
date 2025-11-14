2e69587d0b6a7764864b7027bd636e5a14417c38
brownegm <32085030+brownegm@users.noreply.github.com>
2025-11-13 16:50:37 -0800

    Update test-psiPx.R

--- tests/testthat/test-psiPx.R
+++ tests/testthat/test-psiPx.R
@@ -7,8 +7,12 @@ test_that("Check model type is correct", {
 testthat::test_that("Check Linear model", {
 
   result <- hydrafit::psiPx(model_type = "Linear")(A = 2, B = 3, px = 0.4, max_cond_at = 0)
-
+  result2 <- hydrafit::psiPx(model_type = "Linear")(A = 2, B = 3, px = 0, max_cond_at = 0)
   expect_equal(result$psi.px, -0.6)
   expect_equal(result$max_c, 3)
+
+  #what is the water potential and max cond at 0% loss in conductance?
+  expect_equal(result2$psi.px, 0)
+  expect_equal(result2$max_c, 3)
 
 })
